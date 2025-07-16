049162612201bdc784d52c14fdc0f750e59803da
Joelinho-code <joeltareas.14@gmail.com>
2025-07-16 00:51:11 -0400

    Delete ejemplo directory

--- ejemplo/js/supabase-client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl =  'https://ezpaehiyfqjhzbbvkpss.supabase.co'
-const supabaseAnonKey = process.env.SUPABASE_KEY
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-export default supabase
